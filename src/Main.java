0a5826933e61822c85a5143635ec25e0889b4220
IDFKEMI <IDFKEMI@GMAIL.COM>
2023-08-09 16:54:33 -0600

    Finished Project, accidentally got ahead of myself

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,28 @@ public class Main {
         team1.name = "BlueberryBros";
         team1.players = "Eighteen";
 
+        Team team2 = new Team();
+        team2.name = "StrawberryStrddlers";
+        team2.players = "Eighteen";
+
         Player player1 = new Player();
         player1.name = "Emiliano Rodriguez";
         player1.age = 19;
         player1.position = "Right wing";
         player1.team = team1;
+
+        Match firstGame = new Match();
+        firstGame.team1 = team1;
+        firstGame.team2 = team2;
+        firstGame.score1 = 14;
+        firstGame.score2 = 16;
+        firstGame.winner = team1;
+
+        Tournament tournament = new Tournament();
+        tournament.name = "Tourney1";
+        tournament.teams = "18";
+
+
 
     }
 }
